56d70a450b1de8f20b7c99159ed02d24fcc93caf
Lucianna Tittle <lucianna.roset@gmail.com>
2022-08-09 08:57:09 -0500

    Completed lab.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+  return destructivelyAppendCat;
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+  return destructivelyPrependCat;
+}
+
+function destructivelyRemoveLastCat(name) {
+  cats.pop(name);
+  return destructivelyRemoveLastCat;
+}
+
+function destructivelyRemoveFirstCat(name) {
+  cats.shift(name);
+  return destructivelyRemoveFirstCat;
+}
+
+function appendCat(name) {
+  const newCatsAppend = [...cats, name];
+  return newCatsAppend;
+}
+
+function prependCat(name) {
+  const newCatsPrepend = [name, ...cats];
+  return newCatsPrepend;
+}
+
+function removeLastCat() {
+  const catsSliceEnd = cats.slice(0, -1);
+  return catsSliceEnd;
+}
+
+function removeFirstCat() {
+  const catsSliceBeginning = cats.slice(1);
+  return catsSliceBeginning;
+}
